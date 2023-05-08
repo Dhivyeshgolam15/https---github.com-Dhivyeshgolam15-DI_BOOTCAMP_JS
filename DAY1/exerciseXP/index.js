e1296a29bd92d576e83e400ab10c36515e8efb45
Dhivyeshgolam15 <golamyashveer@gmail.com>
2023-05-08 19:36:40 +0400

    add wrk

--- DAY1/exerciseXP/index.js
+++ DAY1/exerciseXP/index.js
@@ -1,3 +1,4 @@
+
 //*ex1
 // #1.1
 // The value of 'a' inside the funcOne function will be 3.
@@ -50,18 +51,18 @@
 //ex2
 
 const winBattle = () => {
-    return true;
-  }
-  
-  const experiencePoints = winBattle() ? 10 : 1;
-  
-  console.log(experiencePoints);
+  return true;
+}
 
-  //ex3
+const experiencePoints = winBattle() ? 10 : 1;
 
-  const isString = value => typeof value === 'string' ? true : false;
+console.log(experiencePoints);
 
-  console.log(isString('hello')); //true
+//ex3
+
+const isString = value => typeof value === 'string' ? true : false;
+
+console.log(isString('hello')); //true
 console.log(isString([1, 2, 4, 0])); //false
 
 //ex4
@@ -70,14 +71,14 @@ const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 
 // loop through the colors array and display each color with its corresponding number
 for (let i = 0; i < colors.length; i++) {
-  console.log(`${i + 1}# choice is ${colors[i]}.`);
+console.log(`${i + 1}# choice is ${colors[i]}.`);
 }
 
 // check if the array contains the value "Violet"
 if (colors.includes("Violet")) {
-  console.log("Yeah");
+console.log("Yeah");
 } else {
-  console.log("No...");
+console.log("No...");
 }
 
 //ex5}
@@ -87,10 +88,10 @@ const ordinal = ["th", "st", "nd", "rd"];
 
 // loop through the colors array and display each color with its corresponding ordinal number
 for (let i = 0; i < colors.length; i++) {
-  let num = i + 1;
-  let ord = num % 10;
-  let suffix = ord >= 1 && ord <= 3 && (num < 10 || num > 20) ? ordinal[ord] : ordinal[0];
-  console.log(`${num}${suffix} choice is ${colors[i]}.`);
+let num = i + 1;
+let ord = num % 10;
+let suffix = ord >= 1 && ord <= 3 && (num < 10 || num > 20) ? ordinal[ord] : ordinal[0];
+console.log(`${num}${suffix} choice is ${colors[i]}.`);
 }
 //ex6
 
@@ -100,15 +101,15 @@ const expenses = ["+200", "-100", "+146", "+167", "-2900"]; // monthly expenses
 
 // loop through the expenses array and apply VAT to each expense
 for (let i = 0; i < expenses.length; i++) {
-  let expense = parseInt(expenses[i]); // convert expense string to integer
-  let taxedExpense = expense + (expense * VAT); // calculate taxed expense
-  expenses[i] = taxedExpense.toString(); // update expense in the array
+let expense = parseInt(expenses[i]); // convert expense string to integer
+let taxedExpense = expense + (expense * VAT); // calculate taxed expense
+expenses[i] = taxedExpense.toString(); // update expense in the array
 }
 
 // loop through the expenses array and update bank amount
 for (let i = 0; i < expenses.length; i++) {
-  let expense = parseInt(expenses[i]); // convert expense string to integer
-  bankAmount += expense; // update bank amount with taxed expense
+let expense = parseInt(expenses[i]); // convert expense string to integer
+bankAmount += expense; // update bank amount with taxed expense
 }
 
 console.log("Bank amount at the end of the month:", bankAmount.toFixed(2)); // display bank amount with 2 decimal places
