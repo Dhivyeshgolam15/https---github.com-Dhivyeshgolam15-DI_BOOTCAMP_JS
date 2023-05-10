73adce06abaa04a17aa83834413db9a12b6ce518
Dhivyeshgolam15 <golamyashveer@gmail.com>
2023-05-10 11:14:58 +0400

    aad x

---
+++ Day2/ExerciseXP/index.js
@@ -0,0 +1,11 @@
+//ex1
+
+const addNumbers = (num1, num2) => num1 + num2;
+
+//ex2
+
+function convertKilogramstoGrams() {
+    return num * 1000;
+}
+const result = convertKilogramstoGrams(3);
+console.log("result", result);
